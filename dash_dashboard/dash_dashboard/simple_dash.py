5014c1a030dea15aeb04294ef1f3088dedeb4f67
Poranut Chollavorn <pipech@gmail.com>
2019-11-12 08:46:27 +0000

    feat(dashboard): add 12col bar chart

--- dash_dashboard/dash_dashboard/simple_dash.py
+++ dash_dashboard/dash_dashboard/simple_dash.py
@@ -99,28 +99,74 @@ def get_layout():
         ], className='row'),
         html.Div([
             html.Div([
-                html.Div([
-                    html.Div([
-                        'Testing Dashboard'
-                    ], className='card-header'),
-                    html.Div([
-                        dcc.Graph(
-                            id='example-graph3',
-                            figure={
-                                'data': [
-                                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                                ],
-                                'layout': {
-                                    'title': 'Dash Data Visualization'
-                                },
-                            },
-                        ),
-                    ], className='card-body'),
-                ], className='card')
-            ], className='col-md-6'),
-        ], className='row')
+                line_chart(),
+            ], className='col-md-12'),
+        ], className='row'),
     ]
+
+    return layout
+
+
+def line_chart():
+    layout = html.Div([
+        html.Div([
+            'US Export of Plastic Scrap'
+        ], className='card-header'),
+        html.Div([
+            dcc.Graph(
+                figure=go.Figure(
+                    data=[
+                        go.Bar(
+                            x=[
+                                1995, 1996, 1997, 1998, 1999,
+                                2000, 2001, 2002, 2003, 2004,
+                                2005, 2006, 2007, 2008, 2009,
+                                2010, 2011, 2012,
+                            ],
+                            y=[
+                                219, 146, 112, 127, 124, 180,
+                                236, 207, 236, 263, 350, 430,
+                                474, 526, 488, 537, 500, 439
+                            ],
+                            name='Rest of world',
+                            marker=go.bar.Marker(
+                                color='rgb(55, 83, 109)'
+                            )
+                        ),
+                        go.Bar(
+                            x=[
+                                1995, 1996, 1997, 1998, 1999,
+                                2000, 2001, 2002, 2003, 2004,
+                                2005, 2006, 2007, 2008, 2009,
+                                2010, 2011, 2012
+                            ],
+                            y=[
+                                16, 13, 10, 11, 28, 37, 43,
+                                55, 56, 88, 105, 156, 270,
+                                299, 340, 403, 549, 499
+                            ],
+                            name='China',
+                            marker=go.bar.Marker(
+                                color='rgb(26, 118, 255)'
+                            )
+                        )
+                    ],
+                    layout=go.Layout(
+                        showlegend=True,
+                        legend=go.layout.Legend(
+                            x=0,
+                            y=1.0
+                        ),
+                        margin=go.layout.Margin(
+                            l=40, r=0, t=40, b=30
+                        ),
+                    )
+                ),
+                style={'height': 300},
+                id='my-bar-graph'
+            ),
+        ], className='card-body'),
+    ], className='card')
 
     return layout
 
